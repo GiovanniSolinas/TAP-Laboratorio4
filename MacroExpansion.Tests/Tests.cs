bc7320a00281951e6b047bc0ccba4a9cff558662
Giovanni Solinas <giovanni.solinas98@gmail.com>
2020-11-27 14:12:08 +0100

    Aggiungere i file di progetto.

---
+++ MacroExpansion.Tests/Tests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacroExpansion.Tests
+{
+    public class Tests {
+    }
+}
